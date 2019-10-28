f74d3d50a3eaabaf53cae01ee6929a1fbd6bb1ed
Admin <admin@KS-MacBook-Pro.local>
2019-10-28 10:39:10 +0300

    Product detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_workout_myshop/providers/products.dart';
 import 'package:provider/provider.dart';
- 
+
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
 
@@ -14,15 +14,40 @@ class ProductDetailScreen extends StatelessWidget {
     final productId = ModalRoute.of(context).settings.arguments as String;
     final product = Provider.of<Products>(context).findById(productId);
     return Scaffold(
-        appBar: AppBar(
-          title: Text(product.title),
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${product.price}',
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+                width: double.infinity,
+                child: Text(
+                  product.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ))
+          ],
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      
+      ),
     );
   }
 }
