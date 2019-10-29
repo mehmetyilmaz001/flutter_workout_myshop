c0d5872dd7ee40123c7c8906a7ccea528b9aa26d
Mehmet Yılmaz <mehmetyilmaz001@gmail.com>
2019-10-30 00:01:39 +0300

    -

--- lib/screens/edit_product.screen.dart
+++ lib/screens/edit_product.screen.dart
@@ -12,6 +12,7 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
 
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -36,6 +37,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
                 focusNode: _priceFocusNode,
+                onFieldSubmitted: (_){
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,
               ),
             ],
           ),
