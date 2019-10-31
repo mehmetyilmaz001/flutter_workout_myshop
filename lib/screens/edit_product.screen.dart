36b6ccf775c24eb599429561fc20c4a854a0c860
Admin <admin@KS-MacBook-Pro.local>
2019-10-31 14:31:10 +0300

    Loading State 2

--- lib/screens/edit_product.screen.dart
+++ lib/screens/edit_product.screen.dart
@@ -112,17 +112,17 @@ class _EditProductScreenState extends State<EditProductScreen> {
     if (isEditMode()) {
       Provider.of<Products>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
+      Navigator.of(context).pop();
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
           .then((_) {
-        setState(() {
-          _isLoading = false;
-        });
-      });
+            setState(() {
+              _isLoading = false;
+            });
+            Navigator.of(context).pop();
+          });
     }
-
-    Navigator.of(context).pop();
   }
 
   bool isEditMode() {
