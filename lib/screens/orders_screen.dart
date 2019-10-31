8b16dc7ccd1e6eec64d81ccc3a9967b60c4fb30c
Mehmet Yılmaz <mehmetyilmaz001@gmail.com>
2019-10-31 23:35:47 +0300

    Alternative way to displaying items on the screen with future builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,43 +4,33 @@ import 'package:flutter_workout_myshop/widgets/app_drawer.dart';
 import 'package:flutter_workout_myshop/widgets/order_list_item.dart';
 import 'package:provider/provider.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = false;
- 
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Orders'),
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading ? Center(child: CircularProgressIndicator()) : ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderListItem(orderData.orders[i]),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Orders'),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              if (dataSnapshot.error != null) {
+                return Center(child: Text('Error Oucured!'),);
+              } else {
+                return Consumer<Orders>(builder: (ctx, orderData, child) => ListView.builder(
+                  itemCount: orderData.orders.length,
+                  itemBuilder: (ctx, i) => OrderListItem(orderData.orders[i]),
+                ));
+              }
+            }
+          },
+        ));
   }
 }
